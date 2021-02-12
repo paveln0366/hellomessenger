771fd47da84dd434168ff37f5798b51f0aeb19b0
Pavel <paveln0366@gmail.com>
2021-02-12 15:09:00 +0300

    added authentication via google

--- app/src/main/java/com/pavelpotapov/hellomessenger/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/hellomessenger/MainActivity.java
@@ -15,19 +15,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 100;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
 
@@ -84,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 if (value != null) {
                     List<Message> messages = value.toObjects(Message.class);
                     adapter.setMessages(messages);
+                    recyclerViewMessages.smoothScrollToPosition(adapter.getItemCount() - 1);
                 }
             }
         });
@@ -117,8 +125,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(this, user.getEmail(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                if (response != null) {
+                    Toast.makeText(this, "Error: " + response.getError(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+            }
+        }
+
     private void signOut() {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Choose authentication providers
+                            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                            // Create and launch sign-in intent
+                            startActivityForResult(
+                                    AuthUI.getInstance()
+                                            .createSignInIntentBuilder()
+                                            .setAvailableProviders(providers)
+                                            .build(),
+                                    RC_SIGN_IN);
+                        }
+                    }
+                });
     }
 }
